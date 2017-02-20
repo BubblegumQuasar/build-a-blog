81c94b62131f2ee5bc471b2be0fcf6a4a53f9ea2
Sarah Morris <bubblegumquasar@gmail.com>
2017-02-19 20:20:06 -0600

    trying to get changes to Shawn

--- main.py
+++ main.py
@@ -15,11 +15,66 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+# class to set up blog posts:
+
+class Blogpost(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class Handler(webapp2.RequestHandler):
+    blog = db.GqlQuery("SELECT * FROM Blogpost ORDER BY created DESC LIMIT 5")
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(**params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+    
+
+class MainPage(Handler):
+    def render_posts(self, title="", content="", error=""):
+       self.render("main-page.html" title=title, content=content, error=error, blog=blog)
+
+class NewPost(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("submit.html" title=title, content=content, error=error)
+
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+
+        if title and content:
+            a = Blogpost(title=title, content=content)
+            a.put()
+
+            self.redirect("/blog")
+        else:
+            error = "We need both a title and some content!"
+            self.render(title, content, error)
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', MainPage),
+    ('/', NewPost)
 ], debug=True)
+
